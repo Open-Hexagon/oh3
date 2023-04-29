ce79fc4ae2e4e465c9e70bfb44d14a326f9ca1d4
Bauumm <oh4@web.de>
2023-04-29 19:25:55 +0200

    add basic run loop with consistent tick rate

--- main.lua
+++ main.lua
@@ -1,11 +1,64 @@
--- ? Maybe don't override love.run just yet.
+function love.run()
+    -- target frametime
+    local frametime = 1 / 240
+    local start_time = love.timer.getTime()
 
-function love.load()
-end
+    -- enforce aspect ratio by rendering to canvas
+    local aspect_ratio = 16 / 9
+    local scale = { 1, 1 }
+    local screen
+    -- correct aspect ratio initially (before the user resizes the window)
+    love.event.push("resize", love.graphics.getDimensions())
 
-function love.update(dt)
-end
+    -- function is called every frame by love
+    return function()
+        -- update as much as required depending on passed time
+        local current_time = love.timer.getTime()
+        while current_time - start_time >= frametime do
+            start_time = start_time + frametime
 
-function love.draw()
-
+            -- process events
+            love.event.pump()
+            for name, a, b, c, d, e, f in love.event.poll() do
+                if name == "quit" then
+                    return a or 0
+                end
+                if name == "resize" then
+                    if a < b * aspect_ratio then
+                        -- window is too high for the aspect ratio
+                        scale[1] = 1
+                        scale[2] = a / (aspect_ratio * b)
+                    else
+                        -- window is too wide for the aspect ratio
+                        scale[1] = b * aspect_ratio / a
+                        scale[2] = 1
+                    end
+                    -- recreate screen canvas to have the correct size
+                    local width, height = love.graphics.getDimensions()
+                    screen = love.graphics.newCanvas(width * scale[1], height * scale[2])
+                end
+            end
+            -- TODO: update game state
+        end
+        if love.graphics.isActive() then
+            local width, height = love.graphics.getDimensions()
+            -- reset any transformations and make the screen black
+            love.graphics.origin()
+            love.graphics.clear(0, 0, 0, 1)
+            -- can only start rendering once the initial resize event was processed
+            if screen ~= nil then
+                -- render onto the screen
+                love.graphics.setCanvas(screen)
+                -- clear with white for now, so the resizing can be seen in action
+                love.graphics.clear(1, 1, 1, 1)
+                -- TODO: rendering
+                love.graphics.setCanvas()
+                -- render the canvas in the middle of the window
+                love.graphics.origin()
+                love.graphics.translate((width - width * scale[1]) / 2, (height - height * scale[2]) / 2)
+                love.graphics.draw(screen)
+            end
+            love.graphics.present()
+        end
+    end
 end
