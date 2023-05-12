7d4c5002128604a552218e044cfc7c5b8ff25a9a
Bauumm <oh4@web.de>
2023-05-12 13:46:19 +0200

    add pulse

--- compat/game192/init.lua
+++ compat/game192/init.lua
@@ -61,17 +61,57 @@ function public.update(frametime)
     -- TODO: adjust tick rate based on object count
     -- TODO: update flash
     -- TODO: update effects
-    -- TODO: if not dead:
-    --   TODO: update walls
-    --   TODO: update events
-    --   TODO: update time stop
-    --   TODO: update increment
-    --   TODO: update level
-    --   TODO: update beatpulse if not disabled in config
-    --   TODO: update pulse if not disabled in config
-    --   TODO: only update style if not bw mode
-    game.style.update(frametime)
-    -- TODO: if dead: mult rot speed by 0.99
+    -- TODO: if not dead
+    if true then
+        -- TODO: update walls
+        -- TODO: update events
+        if game.status.time_stop <= 0 then
+            game.status.current_time = game.status.current_time + frametime / 60
+            game.status.increment_time = game.status.increment_time + frametime / 60
+        else
+            game.status.time_stop = game.status.time_stop - frametime
+        end
+        if game.status.increment_enabled then
+            if game.status.increment_time >= game.level_data.increment_time then
+                game.status.increment_time = 0
+                -- TODO: increment difficulty
+            end
+        end
+        -- TODO: update level
+        -- TODO: if not beatpulse disabled in config
+        if game.status.beat_pulse_delay <= 0 then
+            game.status.beat_pulse = game.level_data.beat_pulse_max
+            game.status.beat_pulse_delay = game.level_data.beat_pulse_delay_max
+        else
+            game.status.beat_pulse_delay = game.status.beat_pulse_delay - frametime
+        end
+        if game.status.beat_pulse > 0 then
+            game.status.beat_pulse = game.status.beat_pulse - 2 * frametime
+        end
+        -- TODO: radius_min = 75 if beatpulse disabled in config
+        local radius_min = game.level_data.radius_min
+        game.status.radius = radius_min * (game.status.pulse / game.level_data.pulse_min) + game.status.beat_pulse
+        -- TODO: if not pulse disabled in config
+        if game.status.pulse_delay <= 0 and game.status.pulse_delay_half <= 0 then
+            local pulse_add = game.status.pulse_direction > 0 and game.level_data.pulse_speed or -game.level_data.pulse_speed_r
+            local pulse_limit = game.status.pulse_direction > 0 and game.level_data.pulse_max or game.level_data.pulse_min
+            game.status.pulse = game.status.pulse + pulse_add * frametime
+            if (game.status.pulse_direction > 0 and game.status.pulse >= pulse_limit) or (game.status.pulse_direction < 0 and game.status.pulse <= pulse_limit) then
+                game.status.pulse = pulse_limit
+                game.status.pulse_direction = -game.status.pulse_direction
+                game.status.pulse_delay_half = game.level_data.pulse_delay_half_max
+                if game.status.pulse_direction < 0 then
+                    game.status.pulse_delay = game.level_data.pulse_delay_max
+                end
+            end
+        end
+        game.status.pulse_delay = game.status.pulse_delay - frametime
+        game.status.pulse_delay_half = game.status.pulse_delay_half - frametime
+        -- TODO: only update style if not bw mode
+        game.style.update(frametime)
+    else
+        -- TODO: mult rot speed by 0.99
+    end
     -- TODO: update 3d if enabled in config
     -- TODO: update rotation if not disabled in config
     -- TODO: handle level change
@@ -80,8 +120,11 @@ end
 
 function public.draw(screen)
     local width, height = screen:getDimensions()
+    -- do the resize adjustment the old game did after already enforcing our aspect ratio
     local zoom_factor = 1 / math.max(1024 / width, 768 / height)
-    love.graphics.scale(zoom_factor, zoom_factor)
+    -- apply pulse as well
+    local p = game.status.pulse / game.level_data.pulse_min
+    love.graphics.scale(zoom_factor / p, zoom_factor / p)
     love.graphics.rotate(math.rad(current_rotation))
     game.style.compute_colors()
     -- TODO: only if not background disabled in config
