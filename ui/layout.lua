eb4934a5eb3467f6e47da2020ba0a930a875a07a
Syyrion <cuiricky4@gmail.com>
2023-05-21 18:16:56 -0400

    cleanup layout

--- ui/layout.lua
+++ ui/layout.lua
@@ -1,44 +1,41 @@
-local signal = require "anim.signal"
+local signal = require("anim.signal")
 
 local layout = {}
 
-do
-    local zero = signal.new_signal(0)
-    ---A signal that outputs the X coordinate of the LEFT edge of the screen (always 0).
-    layout.LEFT = zero
+---A signal that outputs the X coordinate of the LEFT edge of the screen (always 0).
+layout.LEFT = signal.new_signal(0)
 
-    ---A signal that outputs the Y coordinate of the TOP edge of the screen (always 0).
-    layout.TOP = zero
+---A signal that outputs the Y coordinate of the TOP edge of the screen (always 0).
+layout.TOP = signal.new_signal(0)
 
-    ---A signal that outputs the X coordinate of the RIGHT edge of the screen.
-    layout.RIGHT = function()
-        return layout.width
-    end
+---A signal that outputs the X coordinate of the RIGHT edge of the screen.
+layout.RIGHT = function()
+    return layout.width
+end
 
-    ---A signal that outputs the Y coordinate of the BOTTOM edge of the screen.
-    layout.BOTTOM = function()
-        return layout.height
-    end
+---A signal that outputs the Y coordinate of the BOTTOM edge of the screen.
+layout.BOTTOM = function()
+    return layout.height
+end
 
-    ---A signal that outputs the larger screen dimension.
-    layout.MAJOR = function()
-        return layout.major
-    end
+---A signal that outputs the larger screen dimension.
+layout.MAJOR = function()
+    return layout.major
+end
 
-    ---A signal that outputs the smaller screen dimension.
-    layout.MINOR = function()
-        return layout.minor
-    end
+---A signal that outputs the smaller screen dimension.
+layout.MINOR = function()
+    return layout.minor
+end
 
-    ---A signal that outputs the X coordinate of the CENTER of the screen.
-    layout.CENTER_X = function()
-        return layout.center_x
-    end
+---A signal that outputs the X coordinate of the CENTER of the screen.
+layout.CENTER_X = function()
+    return layout.center_x
+end
 
-    ---A signal that outputs the Y coordinate of the CENTER edge of the screen.
-    layout.CENTER_Y = function()
-        return layout.center_y
-    end
+---A signal that outputs the Y coordinate of the CENTER edge of the screen.
+layout.CENTER_Y = function()
+    return layout.center_y
 end
 
 function layout.resize()
