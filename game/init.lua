c28ae05903ca5141854b0db795cb33a0ba763e40
Bauumm <oh4@web.de>
2023-07-10 14:39:28 +0200

    lay out public game functions to implement for the new game

---
+++ game/init.lua
@@ -0,0 +1,40 @@
+-- TODO: implement functions and the game itself
+local public = {}
+
+---starts a new game
+---@param pack_id string
+---@param level_id string
+---@param level_options table
+function public.start(pack_id, level_id, level_options)
+end
+
+---update the game
+function public.update()
+end
+
+---draw the game to the current canvas
+---@param screen love.Canvas
+function public.draw(screen)
+end
+
+---get the current score
+---@return number
+function public.get_score()
+end
+
+---runs the game until the player dies without caring about real time
+function public.run_game_until_death()
+end
+
+---stop the game
+function public.stop()
+end
+
+---initialize the game
+---@param pack_level_data table
+---@param input_handler table
+---@param config table
+---@param persistent_data table
+---@param audio table
+function public.init(pack_level_data, input_handler, config, persistent_data, audio)
+end
