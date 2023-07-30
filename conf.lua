96477c9f2ef8c1d6918568e0e83c1b9f9046928f
Bauumm <oh4@web.de>
2023-07-30 20:23:06 +0200

    enabled system module in headless as well

--- conf.lua
+++ conf.lua
@@ -32,7 +32,7 @@ function love.conf(t)
         t.modules.mouse = false
         t.modules.physics = false
         t.modules.sound = false
-        t.modules.system = false
+        t.modules.system = true
         t.modules.thread = args.server or args.migrate
         t.modules.timer = true
         t.modules.touch = false
