a983a91326aed31a8205262ae7606f456a5d5f01
Bauumm <oh4@web.de>
2023-12-02 13:37:10 +0100

    fix death overlay not centered

--- ui/overlay/death.lua
+++ ui/overlay/death.lua
@@ -8,42 +8,40 @@ local transitions = require("ui.anim.transitions")
 local death = overlay:new()
 
 death.layout = flex:new({
-    flex:new({
-        quad:new({
-            child_element = label:new("Retry"),
-            selectable = true,
-            selection_handler = function(self)
-                if self.selected then
-                    self.border_color = { 0, 0, 1, 1 }
-                else
-                    self.border_color = { 1, 1, 1, 1 }
-                end
-            end,
-            click_handler = function()
-                game_handler.stop()
-                death:close()
-                game_handler.retry()
-            end,
-        }),
-        quad:new({
-            child_element = label:new("Back"),
-            selectable = true,
-            selection_handler = function(self)
-                if self.selected then
-                    self.border_color = { 0, 0, 1, 1 }
-                else
-                    self.border_color = { 1, 1, 1, 1 }
-                end
-            end,
-            click_handler = function()
-                local ui = require("ui")
-                game_handler.preview_start("", "", {}, false, true)
-                ui.open_screen("levelselect")
-                death:close()
-            end,
-        }),
-    }, { align_items = "center" }),
-}, { direction = "column", align_items = "center" })
+    quad:new({
+        child_element = label:new("Retry"),
+        selectable = true,
+        selection_handler = function(self)
+            if self.selected then
+                self.border_color = { 0, 0, 1, 1 }
+            else
+                self.border_color = { 1, 1, 1, 1 }
+            end
+        end,
+        click_handler = function()
+            game_handler.stop()
+            death:close()
+            game_handler.retry()
+        end,
+    }),
+    quad:new({
+        child_element = label:new("Back"),
+        selectable = true,
+        selection_handler = function(self)
+            if self.selected then
+                self.border_color = { 0, 0, 1, 1 }
+            else
+                self.border_color = { 1, 1, 1, 1 }
+            end
+        end,
+        click_handler = function()
+            local ui = require("ui")
+            game_handler.preview_start("", "", {}, false, true)
+            ui.open_screen("levelselect")
+            death:close()
+        end,
+    }),
+}, { align_items = "center", justify_content = "center" })
 
 death.backdrop = false
 death.transition = transitions.scale
