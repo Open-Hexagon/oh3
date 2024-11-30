9975c5c76f5fc7032f68d2b7ba3eb5dd2eece60d
Bauumm <oh4@web.de>
2024-11-30 12:14:02 +0100

    increase thread data timeout

--- server/game.lua
+++ server/game.lua
@@ -6,11 +6,11 @@ function game.init(render_top_scores)
     thread:start("server.game_thread", true)
     love.thread.getChannel("game_commands"):push({ "set_render_top_scores", render_top_scores })
     love.thread.getChannel("game_commands"):push({ "get_levels21" })
-    game.level_validators = love.thread.getChannel("ranked_levels"):demand(1)
+    game.level_validators = love.thread.getChannel("ranked_levels"):demand(5)
     if not game.level_validators then
         error("failed getting level ids from thread: " .. (thread:getError() or "no error"))
     end
-    game.levels = love.thread.getChannel("ranked_levels"):demand(1)
+    game.levels = love.thread.getChannel("ranked_levels"):demand(5)
     if not game.levels then
         error("failed getting levels from thread: " .. (thread:getError() or "no error"))
     end
