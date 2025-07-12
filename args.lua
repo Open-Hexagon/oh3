61405af25cf9b69842573aad8c23204d52637b3f
Bauumm <oh4@web.de>
2025-07-12 18:30:25 +0200

    use argparse for command line arguments

--- args.lua
+++ args.lua
@@ -5,30 +5,15 @@ if arg == nil then
         headless = true,
     }
 end
-local args = love.arg.parseGameArguments(arg)
-local ret = {}
--- tests are always run in headless mode
-ret.headless = love.filesystem.getIdentity() == "ohtest"
-ret.server = false
-ret.migrate = false
-ret.render = false
-ret.web = false
-for i = 1, #args do
-    if args[i] == "--headless" then
-        ret.headless = true
-    elseif args[i] == "--server" then
-        ret.server = true
-        ret.headless = true
-    elseif args[i] == "--migrate" then
-        ret.headless = true
-        ret.migrate = true
-    elseif args[i] == "--render" then
-        ret.render = true
-        ret.headless = false
-    elseif args[i] == "--web" then
-        ret.web = true
-    else
-        ret.no_option = args[i]
-    end
-end
-return ret
+local argparse = require("extlibs.argparse")
+
+local parser = argparse("oh-ce", "Open Hexagon Community Edition")
+
+parser:argument("replay_file", "Path to replay file."):args("?")
+parser:flag("--headless", "Run the game in headless mode.")
+parser:flag("--server", "Start the game server.")
+parser:option("--migrate", "Steam version server database to migrate to new format.")
+parser:flag("--render", "Render a video of the given replay. Also enables server side replay rendering for #1 scores.")
+parser:flag("--web", "Enables the web api.")
+
+return parser:parse(love.arg.parseGameArguments(arg))
