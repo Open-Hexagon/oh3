22ee25b61979764f9affae4301302ac1c539e188
Bauumm <oh4@web.de>
2025-07-13 21:27:54 +0200

    workflow: fix meson setup not finding lua headers

--- extra/web/lua.hpp
+++
@@ -1,9 +0,0 @@
-// lua.hpp
-// Lua header files for C++
-// <<extern "C">> not supplied automatically because Lua also compiles as C++
-
-extern "C" {
-#include "../../../../megasource-web/libs/lua-5.1.5/src/lua.h"
-#include "../../../../megasource-web/libs/lua-5.1.5/src/lualib.h"
-#include "../../../../megasource-web/libs/lua-5.1.5/src/lauxlib.h"
-}
