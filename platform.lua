cbaafff6eeca78ae762b3a4fb590fcc730071b6e
Bauumm <oh4@web.de>
2025-07-13 11:10:58 +0200

    add debug.getinfo nparams and isvarag compat

--- platform.lua
+++ platform.lua
@@ -17,3 +17,20 @@ if not xpcall_takes_arguments then
         return old_xpcall(function() return fun(unpack(varargs)) end, errfun)
     end
 end
+
+-- PUC lua 5.1 does not return nparams or isvararg from debug.getinfo
+-- instead the data is encoded in string.dump(function).
+if debug.getinfo(function(a, b)end).nparams ~= 2 then
+    local old_getinfo = debug.getinfo
+    debug.getinfo = function (f, what)
+        local res = old_getinfo(f, what)
+        local s = string.dump(f)
+        assert(s:sub(1, 6) == "\27LuaQ\0", "This code works only in Lua 5.1")
+        local int_size = s:byte(8)
+        local ptr_size = s:byte(9)
+        local pos = 14 + ptr_size + (s:byte(7) > 0 and s:byte(13) or s:byte(12 + ptr_size)) + 2 * int_size
+        res.nparams = s:byte(pos)
+        res.isvararg = s:byte(pos + 1) > 0
+        return res
+    end
+end
