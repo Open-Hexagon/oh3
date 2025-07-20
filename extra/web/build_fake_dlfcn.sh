52d5dd7fa09cb25a62f45bafcab810d061e8574f
Bauumm <oh4@web.de>
2025-07-20 19:17:35 +0200

    workflow: forgot header file

--- extra/web/build_fake_dlfcn.sh
+++ extra/web/build_fake_dlfcn.sh
@@ -6,4 +6,4 @@ echo "$( \
 	emcc $SCRIPT_DIR/fake_dlfcn.c -DLIBRARY_TABLES="$( \
 		$SCRIPT_DIR/mk_symtbl.sh cffi.a:libcffi-lua-5.1.a libsqlite3.a:sqlite3.o
 	)" -E
-)" | emcc -xc - -c -o fake_dlfcn.o -O3
+)" | emcc -xc - -c -o fake_dlfcn.o -O3 $@
