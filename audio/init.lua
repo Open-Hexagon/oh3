add863746cd79af16067e8ced7b7a06b02a9009d
Bauumm <oh4@web.de>
2025-07-23 17:27:23 +0200

    revert default audio backend

--- audio/init.lua
+++ audio/init.lua
@@ -1,7 +1,7 @@
 -- TODO: make configurable
 -- (will have to restart when changing, might be different once the asset rework is done)
 if true then
-    return require("audio.queue")
-else
     return require("audio.immediate")
+else
+    return require("audio.queue")
 end
