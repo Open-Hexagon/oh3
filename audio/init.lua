0a8c136f1043f07e1e07913df3281c5fd64d1cab
Bauumm <oh4@web.de>
2025-07-28 00:48:54 +0200

    fix wrong audio implementation when rendering

--- audio/init.lua
+++ audio/init.lua
@@ -1,7 +1,10 @@
+local args = require("args")
+
+
 -- TODO: make configurable
 -- (will have to restart when changing, might be different once the asset rework is done)
-if true then
-    return require("audio.immediate")
-else
+if args.render then
     return require("audio.queue")
+else
+    return require("audio.immediate")
 end
