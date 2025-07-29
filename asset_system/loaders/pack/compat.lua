a5129dc1259c7d44c0a228d3e2a751da34c344f7
Bauumm <oh4@web.de>
2025-07-29 21:35:55 +0200

    add pack loaders

---
+++ asset_system/loaders/pack/compat.lua
@@ -0,0 +1,205 @@
+local log = require("log")(...)
+local index = require("asset_system.index")
+local json = require("extlibs.json.jsonc")
+local vfs = require("compat.game192.virtual_filesystem")
+
+local compat_loaders = {}
+
+---steam version has special unique pack identifiers other than the folder name
+---@param disambiguator string
+---@param author string
+---@param name string
+---@param version number?
+---@return string
+function compat_loaders.build_pack_id21(disambiguator, author, name, version)
+    local pack_id = disambiguator .. "_" .. author .. "_" .. name
+    if version ~= nil then
+        pack_id = pack_id .. "_" .. math.floor(version)
+    end
+    pack_id = pack_id:gsub(" ", "_")
+    return pack_id
+end
+
+function compat_loaders.text_file(path_or_content, use_vfs)
+    if use_vfs then
+        return path_or_content
+    else
+        index.watch_file(path_or_content)
+        return love.filesystem.read(path_or_content)
+    end
+end
+
+function compat_loaders.json_file(path_or_content, use_vfs)
+    local str = index.local_request("pack.compat.text_file", path_or_content, use_vfs)
+    -- not a good way but hardcoding some known cases
+    str = str:gsub(": 00 }", ": 0 }")
+    str = str:gsub(", 00", ", 0")
+    str = str:gsub("%[00,", "%[0,")
+    str = str:gsub("055%]", "55%]")
+    -- remove multiline comments
+    while str:find("/*", 0, true) and str:find("*/", 0, true) do
+        local cstart = str:find("/*", 0, true)
+        local cend = str:find("*/", 0, true)
+        str = str:sub(1, cstart - 1) .. str:sub(cend + 2)
+    end
+    -- catch decode errors
+    local _, result = xpcall(json.decode_jsonc, function(msg)
+        log("Error: can't decode '" .. path_or_content .. "': " .. msg)
+    end, str)
+    return result
+end
+
+function compat_loaders.virtual_folder(pack_folder_name)
+    index.watch("persistent_data:" .. pack_folder_name)
+    local persistent_data = love.thread.getChannel("persistent_data:" .. pack_folder_name):peek()
+    vfs.clear()
+    local virtual_pack_folder
+    if persistent_data then
+        vfs.load_files(json.decode(persistent_data))
+        virtual_pack_folder = vfs.dump_real_files_recurse()[pack_folder_name]
+    end
+    return virtual_pack_folder or {}
+end
+
+local file_loaders = {
+    [".json"] = "pack.compat.json_file",
+}
+
+---iterate over the contents of all files with a certain ending in a certain folder
+---@param dir string
+---@param ending string
+---@param pack_path string
+---@param pack_folder_name string
+---@return function
+local function file_iter(dir, ending, pack_path, pack_folder_name)
+    local loader = file_loaders[ending] or "pack.compat.text_file"
+    local files = love.filesystem.getDirectoryItems(pack_path .. dir)
+    local virt_folder = index.local_request("pack.compat.virtual_folder", pack_folder_name)
+    virt_folder = virt_folder[dir] or {}
+    -- add virtual files to list
+    for file in pairs(virt_folder) do
+        files[#files + 1] = file
+    end
+    return coroutine.wrap(function()
+        for i = 1, #files do
+            local name = files[i]
+            if name:sub(-#ending) == ending then
+                -- virtual files take precedence over real ones (overwriting)
+                if virt_folder[name] then
+                    coroutine.yield(index.local_request(loader, virt_folder[name], true), name)
+                else
+                    coroutine.yield(index.local_request(loader, pack_path .. dir .. "/" .. name), name)
+                end
+            end
+        end
+    end)
+end
+
+function compat_loaders.level_datas(pack_path, pack_folder_name)
+    local levels = {}
+    for level_json in file_iter("Levels", ".json", pack_path, pack_folder_name) do
+        -- make keys have the same name for all versions
+        -- get key names
+        local key_names = {}
+        for key in pairs(level_json) do
+            key_names[#key_names + 1] = key
+        end
+        -- and then translate them to avoid modifying the table while iterating (which may skip some keys)
+        for k = 1, #key_names do
+            local key = key_names[k]
+            local value = level_json[key]
+            local snake_case_key = key:gsub("([a-z])([A-Z])", "%1_%2"):lower()
+            snake_case_key = snake_case_key:gsub("multipliers", "mults")
+            level_json[snake_case_key] = value
+        end
+
+        -- set defaults
+        level_json.id = level_json.id or "nullId"
+        level_json.name = level_json.name or "nullName"
+        level_json.description = level_json.description or ""
+        level_json.author = level_json.author or ""
+        level_json.menu_priority = level_json.menu_priority or 0
+        if level_json.selectable == nil then
+            level_json.selectable = true
+        end
+        level_json.music_id = level_json.music_id or "nullMusicId"
+        level_json.sound_id = level_json.sound_id or "nullSoundId"
+        level_json.style_id = level_json.style_id or "nullStyleId"
+        level_json.lua_file = level_json.lua_file or "nullLuaPath"
+        level_json.difficulty_mults = level_json.difficulty_mults or {}
+        -- add 1x difficulty mult if it doesn't exist
+        local has1 = false
+        for k = 1, #level_json.difficulty_mults do
+            if level_json.difficulty_mults[k] == 1 then
+                has1 = true
+                break
+            end
+        end
+        if not has1 then
+            level_json.difficulty_mults[#level_json.difficulty_mults + 1] = 1
+        end
+        -- sort difficulties
+        table.sort(level_json.difficulty_mults)
+
+        -- insertion sort by menu priority
+        for i = 1, #levels + 1 do
+            if levels[i] == nil or levels[i].menu_priority >= level_json.menu_priority then
+                table.insert(levels, i, level_json)
+                break
+            end
+        end
+    end
+    return levels
+end
+
+function compat_loaders.preload_pack(pack_folder_name, version)
+    local folder = "packs" .. version .. "/" .. pack_folder_name .. "/"
+    local function check_file(file)
+        return love.filesystem.exists(folder .. file)
+    end
+    if check_file("pack.json") and check_file("Scripts") then
+        local pack = index.local_request("pack.compat.json_file", folder .. "pack.json")
+        pack.game_version = version
+        pack.path = folder
+        pack.folder_name = pack_folder_name
+        if version == 21 then
+            -- steam version defaults
+            pack.name = pack.name or "unkown name"
+            pack.author = pack.author or "unkown author"
+            pack.description = pack.description or "no description"
+            pack.version = pack.version or 0
+            pack.priority = pack.priority or 100
+
+            pack.id = compat_loaders.build_pack_id21(pack.disambiguator, pack.author, pack.name, pack.version)
+        else
+            pack.name = pack.name or ""
+            pack.id = pack_folder_name
+        end
+
+        local level_list = index.local_request("pack.compat.level_datas", pack.path, pack.folder_name)
+        pack.levels = {}
+        for i = 1, #level_list do
+            local level = level_list[i]
+            pack.levels[level.id] = level
+        end
+        return pack
+    elseif check_file("pack.json") then
+        log("Invalid pack at '" .. pack_folder_name .. "' missing pack.json")
+    elseif check_file("Scripts") then
+        log("Invalid pack at '" .. pack_folder_name .. "' missing Scripts folder")
+    else
+        log("Invalid pack at '" .. pack_folder_name .. "' missing pack.json and Scripts folder")
+    end
+end
+
+function compat_loaders.load_dependency_map21()
+    local packs21 = index.local_request("pack.preload_packs", 21)
+    local map = {}
+    for i = 1, #packs21 do
+        local pack = packs21[i]
+        map[compat_loaders.build_pack_id21(pack.disambiguator, pack.author, pack.name)] = pack
+    end
+    return map
+end
+
+return compat_loaders
---
+++ asset_system/loaders/pack/init.lua
@@ -0,0 +1,90 @@
+local log = require("log")(...)
+local index = require("asset_system.index")
+local compat = require("asset_system.loaders.pack.compat")
+
+local pack_loaders = {}
+
+function pack_loaders.preload_packs(version)
+    log("Loading pack information for game" .. version)
+    local pack_folders = love.filesystem.getDirectoryItems("packs" .. version)
+    local packs = {}
+    for j = 1, #pack_folders do
+        packs[#packs + 1] = index.local_request("pack.compat.preload_pack", pack_folders[j], version)
+    end
+    return packs
+end
+
+function pack_loaders.level_register(pack_path, pack_folder_name, version)
+    local level_list = index.local_request("pack.compat.level_datas", pack_path, pack_folder_name)
+    local result = {}
+    for i = 1, #level_list do
+        local level = level_list[i]
+        if version ~= 192 or level.selectable then
+            result[#result + 1] = {
+                id = level.id,
+                name = level.name,
+                author = level.author,
+                description = level.description,
+                options = { difficulty_mult = level.difficulty_mults },
+            }
+        end
+    end
+    return result
+end
+
+function pack_loaders.pack_register(pack_folder_name, version)
+    local pack = index.local_request("pack.compat.preload_pack", pack_folder_name, version)
+
+    -- check dependencies and add ids to list
+    local has_all_deps = true
+    local dependency_ids = {}
+    if version == 21 and type(pack.dependencies) == "table" and #pack.dependencies > 0 then
+        local dependency_pack_map21 = index.local_request("pack.compat.load_dependency_map21")
+        for i = 1, #pack.dependencies do
+            local dependency = pack.dependencies[i]
+            local index_pack_id = compat.build_pack_id21(dependency.disambiguator, dependency.author, dependency.name)
+            local dependency_pack = dependency_pack_map21[index_pack_id]
+            if dependency_pack == nil then
+                has_all_deps = false
+            else
+                dependency_ids[#dependency_ids + 1] = dependency_pack.id
+            end
+        end
+    end
+
+    if has_all_deps then
+        local levels = index.local_request("pack.level_register", pack.path, pack.folder_name, version)
+        return {
+            id = pack.id,
+            name = pack.name,
+            folder_name = pack.folder_name,
+            game_version = version,
+            dependency_ids = dependency_ids,
+            levels = levels,
+            level_count = #levels,
+        }
+    else
+        log("Pack with id '" .. pack.id .. "' has unsatisfied dependencies!")
+    end
+end
+
+function pack_loaders.load_register()
+    local result = {}
+    -- check all folders in save directory
+    local folders = love.filesystem.getDirectoryItems("")
+    for i = 1, #folders do
+        -- check if the name matches "packs<version>"
+        local version = tonumber(folders[i]:match("packs(.*)"))
+        if version then
+            -- folder is a pack folder
+            local pack_list = index.local_request("pack.preload_packs", version)
+            for k = 1, #pack_list do
+                local pack = pack_list[k]
+                result[#result + 1] = index.local_request("pack.pack_register", pack.folder_name, version)
+            end
+        end
+    end
+    return result
+end
+
+return pack_loaders
